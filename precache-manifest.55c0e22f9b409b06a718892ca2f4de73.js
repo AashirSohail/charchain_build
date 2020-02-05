1b871622de87d8e23e13e736963a61113d754355
AashirSohail <asohia..bscs16seecs@seecs.edu.pk>
2020-02-05 18:10:37 +0500

    tryna deploy on Netlify

---
+++ precache-manifest.55c0e22f9b409b06a718892ca2f4de73.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9914b5dd9f71e8e3a060567aba6d38e9",
+    "url": "/AashirSohail/carchain/index.html"
+  },
+  {
+    "revision": "9f32e0f79b06e42a8cc4",
+    "url": "/AashirSohail/carchain/static/css/2.6f690ab2.chunk.css"
+  },
+  {
+    "revision": "93564d686188ad4455f9",
+    "url": "/AashirSohail/carchain/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "9f32e0f79b06e42a8cc4",
+    "url": "/AashirSohail/carchain/static/js/2.0c8822a9.chunk.js"
+  },
+  {
+    "revision": "dab28bac8be664d03c5f0a10cfa299e5",
+    "url": "/AashirSohail/carchain/static/js/2.0c8822a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93564d686188ad4455f9",
+    "url": "/AashirSohail/carchain/static/js/main.333e7168.chunk.js"
+  },
+  {
+    "revision": "1c76c8ab7015cc5e066e",
+    "url": "/AashirSohail/carchain/static/js/runtime-main.b4d46241.js"
+  },
+  {
+    "revision": "34a6b8c487101984364c1d50c66ccbf9",
+    "url": "/AashirSohail/carchain/static/media/Aashir.34a6b8c4.png"
+  },
+  {
+    "revision": "2c6d539112566aba10a24ea9bbb719b9",
+    "url": "/AashirSohail/carchain/static/media/Danial.2c6d5391.png"
+  },
+  {
+    "revision": "c7ab9ff6c4257203d0f2428f6ec0e543",
+    "url": "/AashirSohail/carchain/static/media/Hassaan.c7ab9ff6.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/AashirSohail/carchain/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/AashirSohail/carchain/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/AashirSohail/carchain/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/AashirSohail/carchain/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/AashirSohail/carchain/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/AashirSohail/carchain/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/AashirSohail/carchain/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/AashirSohail/carchain/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/AashirSohail/carchain/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/AashirSohail/carchain/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/AashirSohail/carchain/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/AashirSohail/carchain/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/AashirSohail/carchain/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/AashirSohail/carchain/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/AashirSohail/carchain/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/AashirSohail/carchain/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/AashirSohail/carchain/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "3c21d2b3f1d181bd4376e26f77d2864d",
+    "url": "/AashirSohail/carchain/static/media/logo_new.3c21d2b3.png"
+  },
+  {
+    "revision": "1b3b2e3c150f0a5c5b9edaccd5650b44",
+    "url": "/AashirSohail/carchain/static/media/saim.1b3b2e3c.png"
+  }
+]);
